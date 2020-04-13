fba6820076f380d215fbc8b7a940ccec2ded302b
Jonathan Hsu <jonhsu.design@gmail.com>
2020-04-13 11:55:57 -0700

    finished mvp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 //TODO: STEP 1 - Import the useState hook.
-import React from "react";
+import React, { useState } from "react";
 import BottomRow from "./BottomRow";
 import "./App.css";
 
 function App() {
-  //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+  //TODO: STEP 2 - Establish your application's state with some useState hooks.  You'll need one for the home score and another for the away score.
+  const [lions, setLions] = useState(0);
+  const [tigers, setTigers] = useState(0);
 
+  const lionsTD = event => {
+    setLions(lions + 7);
+  }
+
+  const lionsFG = event => {
+    setLions(lions + 3);
+  }
+
+  const tigersTD = event => {
+    setTigers(tigers + 7);
+  }
+
+  const tigersFG = event => {
+    setTigers(tigers + 3);
+  }
+ 
   return (
     <div className="container">
       <section className="scoreboard">
@@ -13,13 +31,13 @@ function App() {
           <div className="home">
             <h2 className="home__name">Lions</h2>
 
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-            <div className="home__score">32</div>
+            {/* TODO STEP 3 - We need to change the hard-coded values in these divs to accept dynamic values from our state. */}
+            <div className="home__score">{lions}</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <div className="away__score">{tigers}</div>
           </div>
         </div>
         <BottomRow />
@@ -28,12 +46,12 @@ function App() {
         <div className="homeButtons">
 
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button className="homeButtons__touchdown" onClick={lionsTD}>Home Touchdown</button>
+          <button className="homeButtons__fieldGoal" onClick={lionsFG}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick={tigersTD}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick={tigersFG}>Away Field Goal</button>
         </div>
       </section>
     </div>
--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 const BottomRow = () => {
+
   return (
     <div className="bottomRow">
       <div className="down">
